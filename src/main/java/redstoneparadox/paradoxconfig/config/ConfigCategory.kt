d6dd904f1c8916b3857f0d522a225e32495292db
RedstoneParadox <spinjitzu00@gmail.com>
2019-10-31 12:14:51 -0700

    Config comments now contain possible values + removed some option functions

--- src/main/java/redstoneparadox/paradoxconfig/config/ConfigCategory.kt
+++ src/main/java/redstoneparadox/paradoxconfig/config/ConfigCategory.kt
@@ -51,49 +51,31 @@ open class ConfigCategory(val key : String = "", val comment: String = "") {
     }
 
     protected fun option(default: Boolean, key: String, comment: String = ""): ConfigOption<Boolean> {
-        val option = ConfigOption(boolType, default, key, comment);
+        val option = ConfigOption(boolType, default, key, "$comment [Values: true/false]")
         optionsMap[key] = option
         return option
     }
 
     protected fun option(default: String, key: String, comment: String = ""): ConfigOption<String> {
-        val option = ConfigOption(stringType, default, key, comment);
-        optionsMap[key] = option
-        return option
-    }
-
-    protected fun option(default: Short, key: String, comment: String = ""): ConfigOption<Short> {
-        val option = ConfigOption(shortType, default, key, comment);
-        optionsMap[key] = option
-        return option
-    }
-
-    protected fun option(default: Int, key: String, comment: String = ""): ConfigOption<Int> {
-        val option = ConfigOption(intType, default, key, comment)
+        val option = ConfigOption(stringType, default, key, "$comment [Values: any string]")
         optionsMap[key] = option
         return option
     }
 
     protected fun option(default: Long, key: String, comment: String = ""): ConfigOption<Long> {
-        val option = ConfigOption(longType, default, key, comment)
+        val option = ConfigOption(longType, default, key, "$comment [Values: any whole number]")
         optionsMap[key] = option
         return option
     }
 
     protected fun option(default: Double, key: String, comment: String = ""): ConfigOption<Double> {
-        val option = ConfigOption(doubleType, default, key, comment)
+        val option = ConfigOption(doubleType, default, key, "$comment [Values: any decimal number]")
         optionsMap[key] = option
         return option
     }
 
-    protected fun rangedOption(default: Int, range: IntRange, key: String, comment: String): RangeConfigOption<Int> {
-        val option = RangeConfigOption(intType, default, key, comment, range)
-        optionsMap[key] = option
-        return option
-    }
-
-    protected fun rangedOption(default: Long, range: LongRange, key: String, comment: String): RangeConfigOption<Long> {
-        val option = RangeConfigOption(longType, default, key, comment, range)
+    protected fun rangedOption(default: Long, range: LongRange, key: String, comment: String = ""): RangeConfigOption<Long> {
+        val option = RangeConfigOption(longType, default, key, "$comment [Values: ${range.first} to ${range.last}]", range)
         optionsMap[key] = option
         return option
     }
@@ -101,8 +83,6 @@ open class ConfigCategory(val key : String = "", val comment: String = "") {
     private companion object {
         val boolType = Boolean::class
         val stringType = String::class
-        val shortType = Short::class
-        val intType = Int::class
         val longType = Long::class
         val doubleType = Double::class
     }
--- src/main/java/redstoneparadox/paradoxconfig/misc/TestConfig.kt
+++ src/main/java/redstoneparadox/paradoxconfig/misc/TestConfig.kt
@@ -3,11 +3,11 @@ package redstoneparadox.paradoxconfig.misc
 import redstoneparadox.paradoxconfig.config.AbstractConfig
 import redstoneparadox.paradoxconfig.config.ConfigCategory
 
-object TestConfig: AbstractConfig("test.json") {
+object TestConfig: AbstractConfig("test.json5") {
 
     val test: Boolean by option(true, "test")
 
-    val testTwo: Long by rangedOption(2L, 1..3L, "other", "")
+    val testTwo: Long by rangedOption(2, 1..3L, "other", "")
 
     object InnerTestConfig: ConfigCategory("inner") {
         val testThree: Boolean by option(false, "third")
