66593ff5cd5dcf4175ee385a98b69b6779c5e253
RedstoneParadox <spinjitzu00@gmail.com>
2019-11-10 18:43:09 -0800

    Cleaned up `compareTo` extension operator for Number

--- src/main/java/redstoneparadox/paradoxconfig/util/Comparision.kt
+++ src/main/java/redstoneparadox/paradoxconfig/util/Comparision.kt
@@ -4,12 +4,14 @@ package redstoneparadox.paradoxconfig.util
  * Created by RedstoneParadox on 11/10/2019.
  */
 
-operator fun Number.compareTo(other: Number): Int {
-    if (this is Byte) return this.compareTo(other.toByte())
-    if (this is Short) return this.compareTo(other.toShort())
-    if (this is Int) return this.compareTo(other.toInt())
-    if (this is Long) return this.compareTo(other.toLong())
-    if (this is Float) return this.compareTo(other.toFloat())
-    if (this is Double) return this.compareTo(other.toDouble())
-    throw Exception("Something strange happened!")
+internal operator fun Number.compareTo(other: Number): Int {
+    return when (this) {
+        is Byte -> this.compareTo(other.toDouble())
+        is Short -> this.compareTo(other.toDouble())
+        is Int -> this.compareTo(other.toDouble())
+        is Long -> this.compareTo(other.toDouble())
+        is Float -> this.compareTo(other.toDouble())
+        is Double -> this.compareTo(other.toDouble())
+        else -> throw Exception("Something strange happened!")
+    }
 }
