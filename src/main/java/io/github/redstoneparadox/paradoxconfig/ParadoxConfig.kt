d639a40426d5dd735151a85ab42fa359f9a3224d
RedstoneParadox <spinjitzu00@gmail.com>
2020-05-23 16:44:18 -0700

    Remove initialization check from entrypoint

--- src/main/java/io/github/redstoneparadox/paradoxconfig/ParadoxConfig.kt
+++ src/main/java/io/github/redstoneparadox/paradoxconfig/ParadoxConfig.kt
@@ -4,6 +4,7 @@ import net.fabricmc.loader.api.FabricLoader
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
 import org.apache.logging.log4j.LogManager
 import io.github.redstoneparadox.paradoxconfig.config.RootConfigCategory
+import io.github.redstoneparadox.paradoxconfig.config.type.OptionType
 import io.github.redstoneparadox.paradoxconfig.util.NewConfigData
 
 object ParadoxConfig: PreLaunchEntrypoint {
@@ -15,20 +16,17 @@ object ParadoxConfig: PreLaunchEntrypoint {
 
     @Suppress("unused")
     override fun onPreLaunch() {
-        if (!initialized) {
-            for (mod in FabricLoader.getInstance().allMods) {
-                if (mod.metadata.containsCustomValue(MODID)) {
-                    val obj = mod.metadata.getCustomValue(MODID).asObject
+        for (mod in FabricLoader.getInstance().allMods) {
+            if (mod.metadata.containsCustomValue(MODID)) {
+                val obj = mod.metadata.getCustomValue(MODID).asObject
 
-                    val rootPackage = obj.get("package").asString
-                    val classNames = obj.get("configs").asArray.map { it.asString }
-                    if (mod.metadata.id == MODID && !FabricLoader.getInstance().isDevelopmentEnvironment) continue
+                val rootPackage = obj.get("package").asString
+                val classNames = obj.get("configs").asArray.map { it.asString }
+                if (mod.metadata.id == MODID && !FabricLoader.getInstance().isDevelopmentEnvironment) continue
 
-                    ConfigManager.initConfig(NewConfigData(rootPackage, classNames, mod.metadata.id))
-                }
+                ConfigManager.initConfig(NewConfigData(rootPackage, classNames, mod.metadata.id))
             }
         }
-        initialized = true
     }
 
     internal fun log(msg: String) {
