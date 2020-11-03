4e3ba5e26bbbe3180a2768c70437b35dbe53c4aa
RedstoneParadox <spinjitzu00@gmail.com>
2020-11-02 20:55:41 -0800

    Fix LibCD compat and bump version number

--- src/main/java/io/github/redstoneparadox/paradoxconfig/conditions/ParadoxConfigCD.kt
+++ src/main/java/io/github/redstoneparadox/paradoxconfig/conditions/ParadoxConfigCD.kt
@@ -1,14 +1,15 @@
 package io.github.redstoneparadox.paradoxconfig.conditions
 
-import blue.endless.jankson.JsonArray
-import blue.endless.jankson.JsonObject
-import blue.endless.jankson.JsonPrimitive
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import io.github.cottonmc.libcd.api.LibCDInitializer
 import io.github.cottonmc.libcd.api.condition.ConditionManager
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.util.Identifier
 import io.github.redstoneparadox.paradoxconfig.ParadoxConfig.CONFIGS
 import io.github.redstoneparadox.paradoxconfig.ParadoxConfig.MODID
+import io.github.redstoneparadox.paradoxconfig.util.ReflectionUtil
 import io.github.redstoneparadox.paradoxconfig.util.compareTo
 
 /**
@@ -19,16 +20,16 @@ object ParadoxConfigCD: LibCDInitializer {
         manager.registerCondition(Identifier(MODID, "option")) {
 
             if (it is JsonObject) {
-                val configID = (it["config"] as? JsonPrimitive)?.value as? String
+                val configID = (it["config"] as? JsonPrimitive)?.asString
 
                 if (!FabricLoader.getInstance().isDevelopmentEnvironment && configID == "${MODID}:test.json5") {
                     return@registerCondition false
                 }
                 else if (configID != null) {
                     val config = CONFIGS[configID]
-                    val value = (it["value"] as? JsonPrimitive)?.value
-                    val predicate = (it["predicate"] as? JsonPrimitive)?.value as? String
-                    val option = (it["option"] as? JsonPrimitive)?.value as? String
+                    val value = (it["value"] as? JsonPrimitive)
+                    val predicate = (it["predicate"] as? JsonPrimitive)?.asString
+                    val option = (it["option"] as? JsonPrimitive)?.asString
 
                     if (config != null && value != null && option != null) {
                         val op = config[option]
@@ -36,10 +37,10 @@ object ParadoxConfigCD: LibCDInitializer {
                             return@registerCondition when (predicate) {
                                 "==" -> op == value
                                 "!=" -> op != value
-                                "<" -> if (op is Number && value is Number) op < value else false
-                                ">" -> if (op is Number && value is Number) op > value else false
-                                "<=" -> if (op is Number && value is Number) op <= value else false
-                                ">=" -> if (op is Number && value is Number) op >= value else false
+                                "<" -> if (op is Number) op < value.asNumber else false
+                                ">" -> if (op is Number) op > value.asNumber else false
+                                "<=" -> if (op is Number) op <= value.asNumber else false
+                                ">=" -> if (op is Number) op >= value.asNumber else false
                                 else -> false
                             }
                         }
@@ -55,23 +56,25 @@ object ParadoxConfigCD: LibCDInitializer {
 
         manager.registerCondition(Identifier(MODID, "contains")) {
             if (it is JsonObject) {
-                val configID = (it["config"] as? JsonPrimitive)?.value as? String
+                val configID = (it["config"] as? JsonPrimitive)?.asString
 
 
                 if (!FabricLoader.getInstance().isDevelopmentEnvironment && configID == "${MODID}:test.json5") {
                     return@registerCondition false
                 } else if (configID != null) {
                     val config = CONFIGS[configID]
-                    val contains = (it["contains"] as? JsonArray)
-                    val option = (it["option"] as? JsonPrimitive)?.value as? String
+                    val contains = (it["contains"] as? JsonArray)?.toList()
+                    val option = (it["option"] as? JsonPrimitive)?.asString
 
                     if (config != null && contains != null && option != null) {
-                        val op = config[option] as? Collection<out Any>
+                        val optionCollection = config[option] as? Collection<out Any>
 
-                        if (op != null && op.size >= contains.size) {
+                        if (optionCollection != null && optionCollection.size >= contains.size) {
                             for (element in contains) {
-                                if (!(element is JsonPrimitive && op.contains(element.value)))
+                                if (!(element is JsonPrimitive && optionCollection.contains(ReflectionUtil.getPrimitiveValue(element)))) {
                                     return@registerCondition false
+                                }
+
                             }
                             return@registerCondition true
                         }
--- src/main/java/io/github/redstoneparadox/paradoxconfig/util/ReflectionUtil.java
+++ src/main/java/io/github/redstoneparadox/paradoxconfig/util/ReflectionUtil.java
@@ -1,6 +1,9 @@
 package io.github.redstoneparadox.paradoxconfig.util;
 
+import com.google.gson.JsonPrimitive;
 import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
 
 public class ReflectionUtil {
 
@@ -9,6 +12,18 @@ public class ReflectionUtil {
         try {
             return Class.forName(name);
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static @Nullable Object getPrimitiveValue(JsonPrimitive jsonPrimitive) {
+        try {
+            Field valueField = jsonPrimitive.getClass().getDeclaredField("value");
+            valueField.setAccessible(true);
+            return valueField.get(jsonPrimitive);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
